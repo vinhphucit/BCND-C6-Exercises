829de7bc2252826a2c24231add3bbca6b8f97d18
vinhphucit <vinhphuc.it@gmail.com>
2021-12-06 21:32:14 +0700

    sdfsadf

--- truffle.js
+++ truffle.js
@@ -1,16 +1,21 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
-// Be sure to match this mnemonic with that in Ganache!
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+// Be sure to match this mnemonic with that in Ganache! 
+var mnemonic = "fade attack defense sound nothing riot rug aim cigar embody until check";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:9545/", 0, 10);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 6700000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.25"
     }
   }
 };
